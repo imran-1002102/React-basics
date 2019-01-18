50c9ddf66c83c0a9f0e4ff8e500db827eb78bcbd
imran-1002102 <imran.eee.cuet@gmail.com>
2019-01-18 16:12:50 +0600

    module 3 complete

--- src/App.js
+++ src/App.js
@@ -3,14 +3,67 @@ import Person from './Person/Person'
 import './App.css';
 
 class App extends Component {
+
+
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 27},
+      {name: 'Stephany', age: 26}
+    ],
+    someOtherPersons: 'someOtherValue'
+  };
+
+  switchNameButtonClickHandler = (newName) => {
+    this.setState({
+      persons: [
+        {name: newName, age: 28},
+        {name: 'Manu', age: 27},
+        {name: 'Stephany', age: 29}
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) =>{
+    this.setState({
+      persons: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 27},
+        {name: 'Stephany', age: 29}
+      ]
+    });
+  }
+
   render() {
+    const buttonStyle = {
+      font: 'inherit',
+      color: 'white',
+      border: '2px solid blue',
+      backgroundColor: 'black',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
-      <h1> Hello There!</h1>
-      <button>Switch Name</button>
-        <Person name = 'Max' age = '26'></Person>
-        <Person name = 'Manu' age = '27'>My Hobbies: Reading books</Person>
-        <Person name = 'Stephani' age = '28'></Person>
+        <h1> Hello There!</h1>
+        <button
+          style = {buttonStyle} 
+          onClick = {this.switchNameButtonClickHandler.bind(this, 'Maximilian')}>Switch Name</button>
+          <Person 
+            name = {this.state.persons[0].name} 
+            age = {this.state.persons[0].age}>
+          </Person>
+          <Person 
+            name = {this.state.persons[1].name} 
+            age = {this.state.persons[1].age}
+            click = {this.switchNameButtonClickHandler.bind(this, 'Max!')}
+            nameChangeHandler = {this.nameChangedHandler}>
+            My Hobbies: Reading books
+          </Person>
+          <Person 
+            name = {this.state.persons[2].name} 
+            age = {this.state.persons[2].age}>
+          </Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,16 @@
 import React from 'react';
-
+import './Person.css';
 
 const person = (props) => {
     return(
-        <div>
-            <p>
+        <div className = 'Person'>
+            <p onClick = {props.click}>
                 I'm {props.name} and I'm {props.age} years old!
             </p>
             <p>
                 {props.children}
             </p>
+            <input type = "text" onChange = {props.nameChangeHandler} value = {props.name}></input>
         </div>
     );
 }
