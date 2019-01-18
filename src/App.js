3fa411b5b200857c2fe04f2679a7c80be5b03085
imran-1002102 <imran.eee.cuet@gmail.com>
2019-01-18 12:55:38 +0600

    person component now displays childern property

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <h1> Hello There!</h1>
+      <button>Switch Name</button>
         <Person name = 'Max' age = '26'></Person>
-        <Person name = 'Manu' age = '27'></Person>
+        <Person name = 'Manu' age = '27'>My Hobbies: Reading books</Person>
         <Person name = 'Stephani' age = '28'></Person>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,10 @@ const person = (props) => {
     return(
         <div>
             <p>
-                I'm {props.name} and I'm {props.age} years of age
+                I'm {props.name} and I'm {props.age} years old!
+            </p>
+            <p>
+                {props.children}
             </p>
         </div>
     );
